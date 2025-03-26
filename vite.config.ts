478859611928d213aa73cd5e32c79221eddb8628
ajayhub21 <rajaybe@gmail.com>
2025-03-26 20:26:25 +0530

    changed site name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sp-Tech", // This should match the repository name or the path
+  base: "/SPTech1.0", // This should match the repository name or the path
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
